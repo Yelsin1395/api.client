acaf449cb7bd56052af7d36ef925e9972fd4a843
Jesús Yelsin Broly <yelgari13@gmail.com>
2022-04-15 21:50:57 -0500

    Fix connect database.

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -9,6 +9,9 @@ const db = new Pool({
   host: process.env.DB_HOST,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export = {
